917caae1abc14ded53d38eae1c98f1d5394376cd
Vladyslav Velychko <VladVell@gmail.com>
2016-08-30 18:22:58 +0300

    Оптимизирован класс Session (убраны лишние клики)

--- fixture/session.py
+++ fixture/session.py
@@ -3,12 +3,11 @@ class SessionHelper:
         self.app = app
 
     def login(self, username, password):
+        #login to main dashboard
         wd = self.app.wd
         self.app.open_home_page()
-        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
