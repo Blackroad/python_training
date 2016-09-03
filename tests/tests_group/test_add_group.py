e7337cc52e205c9d8858ea4b700c6ad50acf316e
Vladyslav Velychko <VladVell@gmail.com>
2016-09-03 16:39:00 +0300

    Создана интеллектуальная фикстура

--- tests/tests_group/test_add_group.py
+++ tests/tests_group/test_add_group.py
@@ -4,12 +4,9 @@ from Model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="new1", header="new2", footer="new3"))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
+
