343686a286c58a417fe0748be0d004af4965d85e
Vladyslav Velychko <VladVell@gmail.com>
2016-09-03 18:19:53 +0300

    Интеллектуальный Login /Logout

--- tests/tests_group/test_add_group.py
+++ tests/tests_group/test_add_group.py
@@ -7,6 +7,7 @@ def test_add_group(app):
     app.group.create(Group(name="new1", header="new2", footer="new3"))
 
 
+
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
 
