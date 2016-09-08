9379627bde0b9184ceb14d07d6839de960fd7ad9
Vladyslav Velychko <VladVell@gmail.com>
2016-09-08 20:55:02 +0300

    Проверка списков в модификации группы

--- tests/tests_group/test_modify_group.py
+++ tests/tests_group/test_modify_group.py
@@ -3,15 +3,20 @@ from Model.group import Group
 
 def test_modify_group_name(app):
     old_groups = app.group.get_group_list()
+    modified_group = Group(name='modified')
+    group = Group(name='real',header = 'true')
+    modified_group.id = old_groups[0].id
     if app.group.count() == 0:
-        app.group.create(Group(name='test',header='test2'))
-    app.group.modify_first_group(Group(name='real'))
+        app.group.create(group)
+    app.group.modify_first_group(modified_group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = modified_group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_group_header(app):
-    old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header='modified'))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+#def test_modify_group_header(app):
+    #old_groups = app.group.get_group_list()
+    #app.group.modify_first_group(Group(header='modified'))
+    #new_groups = app.group.get_group_list()
+    #assert len(old_groups) == len(new_groups)
