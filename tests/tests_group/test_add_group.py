d0aa04d3ed7f15b4fabedc6202a44e1fb7760ece
Vladyslav Velychko <VladVell@gmail.com>
2016-09-08 16:28:57 +0300

    Добавлены сравнения списков групп

--- tests/tests_group/test_add_group.py
+++ tests/tests_group/test_add_group.py
@@ -4,10 +4,16 @@ from Model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="new1", header="new2", footer="new3"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
--- tests/tests_group/test_modify_group.py
+++ tests/tests_group/test_modify_group.py
@@ -2,10 +2,16 @@ from Model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name='test',header='test2'))
     app.group.modify_first_group(Group(name='real'))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header='modified'))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
