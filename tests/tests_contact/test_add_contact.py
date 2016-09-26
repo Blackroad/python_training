6cf97f4fc7b61a400d4be7e1a73ed7ec600e4d2c
Vladyslav Velychko <VladVell@gmail.com>
2016-09-26 21:25:43 +0300

    Копипаст исправление

--- tests/tests_contact/test_add_contact.py
+++ tests/tests_contact/test_add_contact.py
@@ -4,19 +4,17 @@ import pytest
 import random
 import string
 
+
 def random_string(prefix,maxlen,symbols=None,digits=None):
     if symbols != None:
         symbols = string.ascii_letters + " "*10
         return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
     elif digits !=None:
-        digits = string.digits + "-"*3 + "+"
+        digits = string.digits + "-"*3
         return prefix + "".join([random.choice(digits) for i in range(random.randrange(maxlen))])
     else:
         all = string.ascii_letters + string.digits +  string.punctuation + " "*10
         return prefix + "".join([random.choice(all) for i in range(random.randrange(maxlen))])
-
-
-
 
 testdata = [Contacts(first_name=random_string('name',10),last_name=random_string('s_name',7),
                      nickname=random_string('n_name',6,symbols=1),
