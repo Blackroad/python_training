6e9ab210fb922d1dac1dcd71342d58cfdefdf7f3
Vladyslav Velychko <VladVell@gmail.com>
2016-10-09 13:50:31 +0300

    Исправлена проверка удаления контактов

--- tests/tests_contact/test_del_contact.py
+++ tests/tests_contact/test_del_contact.py
@@ -9,9 +9,9 @@ def test_del_contact(app,db,check_ui):
     old_contacts = db.get_contact_list()
     contact = random.choice(old_contacts)
     app.contacts.delete_contact_by_id(contact.id)
-    new_contacts = db.get_contact_list()
     assert len(old_contacts) - 1 == app.contacts.count()
     old_contacts.remove(contact)
+    new_contacts = db.get_contact_list()
     assert old_contacts == new_contacts
     if check_ui:
         assert sorted(new_contacts, key=Contacts.id_or_max) == sorted(app.contacts.get_group_list(), key=Contacts.id_or_max)
